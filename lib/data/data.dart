ec29d1d02a0e061df7b8aa852a992a3ed38143ae
saicharan vissapragada <saicharanvissapragada@saicharans-MacBook-Air.local>
2023-01-09 20:15:09 +0530

    Initial commit

---
+++ lib/data/data.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_neflix/assets.dart';
+import 'package:flutter_neflix/models/content_model.dart';
+
+const Content sintelContent = Content(
+  name: 'Sintel',
+  imageUrl: Assets.sintel,
+  titleImageUrl: Assets.sintelTitle,
+  videoUrl: Assets.sintelVideoUrl,
+  description:
+      'A lonely young woman, Sintel, helps and befriends a dragon,\nwhom she calls Scales. But when he is kidnapped by an adult\ndragon, Sintel decides to embark on a dangerous quest to find\nher lost friend Scales.', color: Colors.black54,
+);
+
+const List<Content> previews = [
+  Content(
+    name: 'Avatar The Last Airbender',
+    imageUrl: Assets.atla,
+    color: Colors.orange,
+    titleImageUrl: Assets.atlaTitle, videoUrl: '', description: '',
+  ),
+  Content(
+    name: 'The Crown',
+    imageUrl: Assets.crown,
+    color: Colors.red,
+    titleImageUrl: Assets.crownTitle, videoUrl: '', description: '',
+  ),
+  Content(
+    name: 'The Umbrella Academy',
+    imageUrl: Assets.umbrellaAcademy,
+    color: Colors.yellow,
+    titleImageUrl: Assets.umbrellaAcademyTitle, videoUrl: '', description: '',
+  ),
+  Content(
+    name: 'Carole and Tuesday',
+    imageUrl: Assets.caroleAndTuesday,
+    color: Colors.lightBlueAccent,
+    titleImageUrl: Assets.caroleAndTuesdayTitle, videoUrl: '', description: '',
+  ),
+  Content(
+    name: 'Black Mirror',
+    imageUrl: Assets.blackMirror,
+    color: Colors.green,
+    titleImageUrl: Assets.blackMirrorTitle, videoUrl: '', description: '',
+  ),
+  Content(
+    name: 'Avatar The Last Airbender',
+    imageUrl: Assets.atla,
+    color: Colors.orange,
+    titleImageUrl: Assets.atlaTitle, videoUrl: '', description: '',
+  ),
+  Content(
+    name: 'The Crown',
+    imageUrl: Assets.crown,
+    color: Colors.red,
+    titleImageUrl: Assets.crownTitle, videoUrl: '', description: '',
+  ),
+  Content(
+    name: 'The Umbrella Academy',
+    imageUrl: Assets.umbrellaAcademy,
+    color: Colors.yellow,
+    titleImageUrl: Assets.umbrellaAcademyTitle, videoUrl: '', description: '',
+  ),
+  Content(
+    name: 'Carole and Tuesday',
+    imageUrl: Assets.caroleAndTuesday,
+    color: Colors.lightBlueAccent,
+    titleImageUrl: Assets.caroleAndTuesdayTitle, videoUrl: '', description: '',
+  ),
+  Content(
+    name: 'Black Mirror',
+    imageUrl: Assets.blackMirror,
+    color: Colors.green,
+    titleImageUrl: Assets.blackMirrorTitle, videoUrl: '', description: '',
+  ),
+];
+
+const List<Content> myList = [
+  Content(name: 'Violet Evergarden', imageUrl: Assets.violetEvergarden, titleImageUrl: '', videoUrl: '',description: '', color: Colors.black12, ),
+  Content(name: 'How to Sell Drugs Online (Fast)', imageUrl: Assets.htsdof, titleImageUrl: '', videoUrl: '',description: '', color: Colors.black12,),
+  Content(name: 'Kakegurui', imageUrl: Assets.kakegurui,titleImageUrl: '', videoUrl: '',description: '', color: Colors.black12,),
+  Content(name: 'Carole and Tuesday', imageUrl: Assets.caroleAndTuesday, titleImageUrl: '', videoUrl: '',description: '', color: Colors.black12,),
+  Content(name: 'Black Mirror', imageUrl: Assets.blackMirror,titleImageUrl: '', videoUrl: '',description: '', color: Colors.black12,),
+  Content(name: 'Violet Evergarden', imageUrl: Assets.violetEvergarden, titleImageUrl: '', videoUrl: '',description: '', color: Colors.black12,),
+  Content(name: 'How to Sell Drugs Online (Fast)', imageUrl: Assets.htsdof, titleImageUrl: '', videoUrl: '',description: '', color: Colors.black12,),
+  Content(name: 'Kakegurui', imageUrl: Assets.kakegurui, titleImageUrl: '', videoUrl: '',description: '', color: Colors.black12,),
+  Content(name: 'Carole and Tuesday', imageUrl: Assets.caroleAndTuesday, titleImageUrl: '', videoUrl: '',description: '', color: Colors.black12,),
+  Content(name: 'Black Mirror', imageUrl: Assets.blackMirror, titleImageUrl: '', videoUrl: '',description: '', color: Colors.black12,),
+];
+
+const List<Content> originals = [
+  Content(name: 'Stranger Things', imageUrl: Assets.strangerThings,titleImageUrl: '', videoUrl: '',description: '', color: Colors.black12,),
+  Content(name: 'The Witcher', imageUrl: Assets.witcher, titleImageUrl: '', videoUrl: '',description: '', color: Colors.black12,),
+  Content(name: 'The Umbrella Academy', imageUrl: Assets.umbrellaAcademy, titleImageUrl: '', videoUrl: '',description: '', color: Colors.black12,),
+  Content(name: '13 Reasons Why', imageUrl: Assets.thirteenReasonsWhy, titleImageUrl: '', videoUrl: '',description: '', color: Colors.black12,),
+  Content(name: 'The End of the F***ing World', imageUrl: Assets.teotfw, titleImageUrl: '', videoUrl: '',description: '', color: Colors.black12,),
+  Content(name: 'Stranger Things', imageUrl: Assets.strangerThings, titleImageUrl: '', videoUrl: '',description: '', color: Colors.black12,),
+  Content(name: 'The Witcher', imageUrl: Assets.witcher, titleImageUrl: '', videoUrl: '',description: '', color: Colors.black12,),
+  Content(name: 'The Umbrella Academy', imageUrl: Assets.umbrellaAcademy, titleImageUrl: '', videoUrl: '',description: '', color: Colors.black12,),
+  Content(name: '13 Reasons Why', imageUrl: Assets.thirteenReasonsWhy, titleImageUrl: '', videoUrl: '',description: '', color: Colors.black12,),
+  Content(name: 'The End of the F***ing World', imageUrl: Assets.teotfw, titleImageUrl: '', videoUrl: '',description: '', color: Colors.black12,),
+];
+
+const List<Content> trending = [
+  Content(name: 'Explained', imageUrl: Assets.explained, titleImageUrl: '', videoUrl: '',description: '', color: Colors.black12,),
+  Content(name: 'Avatar The Last Airbender', imageUrl: Assets.atla, titleImageUrl: '', videoUrl: '',description: '', color: Colors.black12,),
+  Content(name: 'Tiger King', imageUrl: Assets.tigerKing, titleImageUrl: '', videoUrl: '',description: '', color: Colors.black12,),
+  Content(name: 'The Crown', imageUrl: Assets.crown, titleImageUrl: '', videoUrl: '',description: '', color: Colors.black12,),
+  Content(name: 'Dogs', imageUrl: Assets.dogs, titleImageUrl: '', videoUrl: '',description: '', color: Colors.black12,),
+  Content(name: 'Explained', imageUrl: Assets.explained, titleImageUrl: '', videoUrl: '',description: '', color: Colors.black12,),
+  Content(name: 'Avatar The Last Airbender', imageUrl: Assets.atla, titleImageUrl: '', videoUrl: '',description: '', color: Colors.black12,),
+  Content(name: 'Tiger King', imageUrl: Assets.tigerKing, titleImageUrl: '', videoUrl: '',description: '', color: Colors.black12,),
+  Content(name: 'The Crown', imageUrl: Assets.crown, titleImageUrl: '', videoUrl: '',description: '', color: Colors.black12,),
+  Content(name: 'Dogs', imageUrl: Assets.dogs, titleImageUrl: '', videoUrl: '',description: '', color: Colors.black12,),
+];
